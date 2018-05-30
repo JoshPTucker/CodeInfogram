e94c9044cb69e3eebf0728e70568c913e3b6ddde
JoshPTucker <jptucker2009@gmail.com>
2018-05-30 11:11:05 -0400

    third commit

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,8 +22,13 @@ public class MainController {
 		model.addAttribute("student", new Student());
 		return "studentform";
 	}
+	
 	@PostMapping("/studentform")
-	public String displayform() {
-		return "";
+	public String displayform( @Valid@ModelAttribute("student")Student student, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "";
+		}else {
+			return"";
+		}
 	}
 }
