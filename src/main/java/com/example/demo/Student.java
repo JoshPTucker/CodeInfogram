97142fa6f5757ad0a743b0119de306819ffb74c2
JoshPTucker <jptucker2009@gmail.com>
2018-06-19 16:24:18 -0400

    added more values to form

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -67,390 +67,291 @@ public class Student {
 	
 	private boolean attenMcb4;
 	private boolean hasDisability;
-	private boolean isCitizen;
-	private boolean isPermanentResidnetAlien;
-	private boolean isEmployed;
-	private boolean isUnderEmployed;
-	private boolean isPellgrant;
-	private boolean isFullTimeStudent;
-	private boolean isPartTimeStudent;
+	private boolean Citizen;
+	private boolean PermanentResidnetAlien;
+	private boolean Employed;
+	private boolean UnderEmployed;
+
+	private boolean Pellgrant;
+	private boolean FullTimeStudent;
+	private boolean PartTimeStudent;
 	//TAAP is Trade adjustment Assistance Program
-	private boolean isTAAP;
-	private boolean isVeteran;
-	private boolean isSpouseVeteran;
+	private boolean recTAAP;
+	private boolean Veteran;
+	private boolean SpouseVeteran;
 	
 	
 	
-	private boolean isNatGaurd;
-	private boolean isMdRes;
-	private boolean is60plus;
+	private boolean NatGaurd;
+	private boolean MdRes;
+	private boolean sixtyPlus;
 	
 	//Has student recieved the Greivance and Veteran forms
 	private boolean recievGreiveVet;
 	
 	
 	//Race choices
-	 private boolean isHispanic;
-	 private boolean isAmericanIndian;
-	 private boolean isAsian;
-	 private boolean isBlackAfricanAmerican;
-	 private boolean isWhite;
-	 private boolean isHawaiian;
+	 private boolean Hispanic;
+	 private boolean AmericanIndian;
+	 private boolean Asian;
+	 private boolean BlackAfricanAmerican;
+	 private boolean White;
+	 private boolean Hawaiian;
 	 
 	 
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getFirstname() {
 		return firstname;
 	}
-
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-
 	public String getLastname() {
 		return lastname;
 	}
-
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-
-//	public String getDob() {
-//		return dob;
-//	}
-//
-//	public void setDob(String dob) {
-//		this.dob = dob;
-//	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	public String getStudentId() {
-		return studentId;
-	}
-
-	public void setStudentId(String studentId) {
-		this.studentId = studentId;
-	}
-
-//	public int getSsn() {
-//		return ssn;
-//	}
-//
-//	public void setSsn(int ssn) {
-//		this.ssn = ssn;
-//	}
-
 	public String getMiddleInitial() {
 		return middleInitial;
 	}
-
 	public void setMiddleInitial(String middleInitial) {
 		this.middleInitial = middleInitial;
 	}
-
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
 	public String getAdress() {
 		return adress;
 	}
-
 	public void setAdress(String adress) {
 		this.adress = adress;
 	}
-
 	public String getZipcode() {
 		return zipcode;
 	}
-
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
-
 	public String getHomephone() {
 		return homephone;
 	}
-
 	public void setHomephone(String homephone) {
 		this.homephone = homephone;
 	}
-
 	public String getState() {
 		return State;
 	}
-
 	public void setState(String state) {
 		State = state;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public String getHighestEducation() {
 		return highestEducation;
 	}
-
 	public void setHighestEducation(String highestEducation) {
 		this.highestEducation = highestEducation;
 	}
-
-	public String getEmployer() {
-		return employer;
-	}
-
-	public void setEmployer(String employer) {
-		this.employer = employer;
-	}
-
-	public String getHours() {
-		return hours;
-	}
-
-	public void setHours(String hours) {
-		this.hours = hours;
-	}
-
-	public String getSalary() {
-		return salary;
-	}
-
-	public void setSalary(String salary) {
-		this.salary = salary;
-	}
-
-	public String getStartdate() {
-		return startdate;
-	}
-
-	public void setStartdate(String startdate) {
-		this.startdate = startdate;
-	}
-
-	public String getWorkPhone() {
-		return workPhone;
-	}
-
-	public void setWorkPhone(String workPhone) {
-		this.workPhone = workPhone;
-	}
-
-	public boolean isHasDisability() {
-		return hasDisability;
-	}
-
-	public void setHasDisability(boolean hasDisability) {
-		this.hasDisability = hasDisability;
-	}
-
-	public boolean isCitizen() {
-		return isCitizen;
-	}
-
-	public void setCitizen(boolean isCitizen) {
-		this.isCitizen = isCitizen;
-	}
-
-	public boolean isPermanentResidnetAlien() {
-		return isPermanentResidnetAlien;
-	}
-
-	public void setPermanentResidnetAlien(boolean isPermanentResidnetAlien) {
-		this.isPermanentResidnetAlien = isPermanentResidnetAlien;
-	}
-
-	public boolean isEmployed() {
-		return isEmployed;
-	}
-
-	public void setEmployed(boolean isEmployed) {
-		this.isEmployed = isEmployed;
-	}
-
-	public boolean isUnderEmployed() {
-		return isUnderEmployed;
-	}
-
-	public void setUnderEmployed(boolean isUnderEmployed) {
-		this.isUnderEmployed = isUnderEmployed;
-	}
-
-	public boolean isPellgrant() {
-		return isPellgrant;
-	}
-
-	public void setPellgrant(boolean isPellgrant) {
-		this.isPellgrant = isPellgrant;
-	}
-
-	public boolean isFullTimeStudent() {
-		return isFullTimeStudent;
-	}
-
-	public void setFullTimeStudent(boolean isFullTimeStudent) {
-		this.isFullTimeStudent = isFullTimeStudent;
-	}
-
-	public boolean isPartTimeStudent() {
-		return isPartTimeStudent;
-	}
-
-	public void setPartTimeStudent(boolean isPartTimeStudent) {
-		this.isPartTimeStudent = isPartTimeStudent;
-	}
-
-	public boolean isTAAP() {
-		return isTAAP;
-	}
-
-	public void setTAAP(boolean isTAAP) {
-		this.isTAAP = isTAAP;
-	}
-
-	public boolean isVeteran() {
-		return isVeteran;
-	}
-
-	public void setVeteran(boolean isVeteran) {
-		this.isVeteran = isVeteran;
-	}
-
-	public boolean isSpouseVeteran() {
-		return isSpouseVeteran;
-	}
-
-	public void setSpouseVeteran(boolean isSpouseVeteran) {
-		this.isSpouseVeteran = isSpouseVeteran;
-	}
-
-	public boolean isNatGaurd() {
-		return isNatGaurd;
-	}
-
-	public void setNatGaurd(boolean isNatGaurd) {
-		this.isNatGaurd = isNatGaurd;
-	}
-
-	public boolean isMdRes() {
-		return isMdRes;
-	}
-
-	public void setMdRes(boolean isMdRes) {
-		this.isMdRes = isMdRes;
-	}
-
-	public boolean isIs60plus() {
-		return is60plus;
-	}
-
-	public void setIs60plus(boolean is60plus) {
-		this.is60plus = is60plus;
-	}
-
-	public boolean isHispanic() {
-		return isHispanic;
-	}
-
-	public void setHispanic(boolean isHispanic) {
-		this.isHispanic = isHispanic;
-	}
-
-	public boolean isAmericanIndian() {
-		return isAmericanIndian;
-	}
-
-	public void setAmericanIndian(boolean isAmericanIndian) {
-		this.isAmericanIndian = isAmericanIndian;
-	}
-
-	public boolean isAsian() {
-		return isAsian;
-	}
-
-	public void setAsian(boolean isAsian) {
-		this.isAsian = isAsian;
-	}
-
-	public boolean isBlackAfricanAmerican() {
-		return isBlackAfricanAmerican;
-	}
-
-	public void setBlackAfricanAmerican(boolean isBlackAfricanAmerican) {
-		this.isBlackAfricanAmerican = isBlackAfricanAmerican;
-	}
-
-	public boolean isWhite() {
-		return isWhite;
-	}
-
-	public void setWhite(boolean isWhite) {
-		this.isWhite = isWhite;
-	}
-
-	public boolean isHawaiian() {
-		return isHawaiian;
-	}
-
-	public void setHawaiian(boolean isHawaiian) {
-		this.isHawaiian = isHawaiian;
-	}
-
-	public String getReasonUEmploy() {
-		return reasonUEmploy;
-	}
-
-	public void setReasonUEmploy(String reasonUEmploy) {
-		this.reasonUEmploy = reasonUEmploy;
-	}
-
 	public String getCurrentEnroll() {
 		return currentEnroll;
 	}
-
 	public void setCurrentEnroll(String currentEnroll) {
 		this.currentEnroll = currentEnroll;
 	}
-
+	public String getEmployer() {
+		return employer;
+	}
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+	public String getHours() {
+		return hours;
+	}
+	public void setHours(String hours) {
+		this.hours = hours;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
+	}
+	public String getWorkPhone() {
+		return workPhone;
+	}
+	public void setWorkPhone(String workPhone) {
+		this.workPhone = workPhone;
+	}
+	public String getReasonUEmploy() {
+		return reasonUEmploy;
+	}
+	public void setReasonUEmploy(String reasonUEmploy) {
+		this.reasonUEmploy = reasonUEmploy;
+	}
 	public boolean isAttenMcb4() {
 		return attenMcb4;
 	}
-
 	public void setAttenMcb4(boolean attenMcb4) {
 		this.attenMcb4 = attenMcb4;
 	}
-
+	public boolean isHasDisability() {
+		return hasDisability;
+	}
+	public void setHasDisability(boolean hasDisability) {
+		this.hasDisability = hasDisability;
+	}
+	public boolean isCitizen() {
+		return Citizen;
+	}
+	public void setCitizen(boolean citizen) {
+		Citizen = citizen;
+	}
+	public boolean isPermanentResidnetAlien() {
+		return PermanentResidnetAlien;
+	}
+	public void setPermanentResidnetAlien(boolean permanentResidnetAlien) {
+		PermanentResidnetAlien = permanentResidnetAlien;
+	}
+	public boolean isEmployed() {
+		return Employed;
+	}
+	public void setEmployed(boolean employed) {
+		Employed = employed;
+	}
+	public boolean isUnderEmployed() {
+		return UnderEmployed;
+	}
+	public void setUnderEmployed(boolean underEmployed) {
+		UnderEmployed = underEmployed;
+	}
+	public boolean isPellgrant() {
+		return Pellgrant;
+	}
+	public void setPellgrant(boolean pellgrant) {
+		Pellgrant = pellgrant;
+	}
+	public boolean isFullTimeStudent() {
+		return FullTimeStudent;
+	}
+	public void setFullTimeStudent(boolean fullTimeStudent) {
+		FullTimeStudent = fullTimeStudent;
+	}
+	public boolean isPartTimeStudent() {
+		return PartTimeStudent;
+	}
+	public void setPartTimeStudent(boolean partTimeStudent) {
+		PartTimeStudent = partTimeStudent;
+	}
+	public boolean isRecTAAP() {
+		return recTAAP;
+	}
+	public void setRecTAAP(boolean recTAAP) {
+		this.recTAAP = recTAAP;
+	}
+	public boolean isVeteran() {
+		return Veteran;
+	}
+	public void setVeteran(boolean veteran) {
+		Veteran = veteran;
+	}
+	public boolean isSpouseVeteran() {
+		return SpouseVeteran;
+	}
+	public void setSpouseVeteran(boolean spouseVeteran) {
+		SpouseVeteran = spouseVeteran;
+	}
+	public boolean isNatGaurd() {
+		return NatGaurd;
+	}
+	public void setNatGaurd(boolean natGaurd) {
+		NatGaurd = natGaurd;
+	}
+	public boolean isMdRes() {
+		return MdRes;
+	}
+	public void setMdRes(boolean mdRes) {
+		MdRes = mdRes;
+	}
+	public boolean isSixtyPlus() {
+		return sixtyPlus;
+	}
+	public void setSixtyPlus(boolean sixtyPlus) {
+		this.sixtyPlus = sixtyPlus;
+	}
 	public boolean isRecievGreiveVet() {
 		return recievGreiveVet;
 	}
-
 	public void setRecievGreiveVet(boolean recievGreiveVet) {
 		this.recievGreiveVet = recievGreiveVet;
 	}
+	public boolean isHispanic() {
+		return Hispanic;
+	}
+	public void setHispanic(boolean hispanic) {
+		Hispanic = hispanic;
+	}
+	public boolean isAmericanIndian() {
+		return AmericanIndian;
+	}
+	public void setAmericanIndian(boolean americanIndian) {
+		AmericanIndian = americanIndian;
+	}
+	public boolean isAsian() {
+		return Asian;
+	}
+	public void setAsian(boolean asian) {
+		Asian = asian;
+	}
+	public boolean isBlackAfricanAmerican() {
+		return BlackAfricanAmerican;
+	}
+	public void setBlackAfricanAmerican(boolean blackAfricanAmerican) {
+		BlackAfricanAmerican = blackAfricanAmerican;
+	}
+	public boolean isWhite() {
+		return White;
+	}
+	public void setWhite(boolean white) {
+		White = white;
+	}
+	public boolean isHawaiian() {
+		return Hawaiian;
+	}
+	public void setHawaiian(boolean hawaiian) {
+		Hawaiian = hawaiian;
+	}
+	 
+	 
 
-	
 }
