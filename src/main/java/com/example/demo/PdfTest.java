35ca5d3b6f941578b4fbeba770da587d2b53e7f9
GBTC441008UR <GBTC441008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-06-27 10:18:51 -0400

    Text now appears in boxes, boxes need borders

--- src/main/java/com/example/demo/PdfTest.java
+++ src/main/java/com/example/demo/PdfTest.java
@@ -16,6 +16,8 @@ import com.itextpdf.kernel.pdf.action.PdfAction;
 import com.itextpdf.kernel.pdf.annot.PdfAnnotation;
 import com.itextpdf.kernel.pdf.annot.PdfTextAnnotation;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.test.annotations.type.SampleTest;
 import org.junit.experimental.categories.Category;
 
@@ -30,15 +32,28 @@ public class PdfTest {
         String src = "static/CyberRegForm.pdf";
         String dest = home+"/Downloads/"+studentName+"RegForm.pdf";
         try {
+            //Creates a new pdf doc
             PdfDocument pdfDoc;
                 pdfDoc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
-            // add content
-            PdfCanvas canvas = new PdfCanvas(pdfDoc.getPage(1));
-            Color greenColor = new DeviceCmyk(1.f, 0.f, 1.f, 0.176f);
-            canvas.setFillColor(greenColor);
-            canvas.rectangle(104, 649, 152.82, 22);
-            canvas.fillStroke();
+            // Adds content to page
 
+            //Handles student id
+            Paragraph paragraph1 = new Paragraph(studentName);
+            Rectangle rectangle1 = new Rectangle(104, 649, 152, 22);
+            PdfCanvas pdfcanvas = new PdfCanvas(pdfDoc.getPage(1));
+            pdfcanvas.saveState().setFillColor(Color.WHITE).rectangle(rectangle1).fill().restoreState();
+            Canvas canvas1 = new Canvas(pdfcanvas,pdfDoc,rectangle1);
+            canvas1.add(paragraph1);
+            canvas1.close();
+
+            //Handles B-Day
+            Paragraph paragraph2 = new Paragraph(student.getLastname());
+            Rectangle rectangle2 = new Rectangle(300, 649, 152, 22);
+            PdfCanvas pdfcanvas2 = new PdfCanvas(pdfDoc.getPage(1));
+            pdfcanvas2.saveState().setFillColor(Color.WHITE).rectangle(rectangle2).fill().restoreState();
+            Canvas canvas2 = new Canvas(pdfcanvas2,pdfDoc,rectangle2);
+            canvas2.add(paragraph2);
+            canvas2.close();
             pdfDoc.close();
             } catch (IOException e) {
                 e.printStackTrace();
