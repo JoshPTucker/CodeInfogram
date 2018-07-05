d939ca79667acf74aaa16e93b2fe9e01d949af4f
GBTC441008UR <GBTC441008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-05 17:01:09 -0400

    controller method to return file  to browser

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Controller
 public class MainController {
@@ -36,5 +48,49 @@ public class MainController {
 			//model.addAttribute(student);
 			return"printRegistration";
 		}
+	}
+	@RequestMapping(value = "/download", method = RequestMethod.GET)
+	public ResponseEntity download(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String filePath = "/downloads";
+
+		try {
+			// Get the text that will be added to the PDF
+			String text = request.getParameter("text");
+			if (text == null || text.trim().length() == 0) {
+				text = "You didn't enter any text.";
+			}
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PdfDocument pdfDoc = new PdfDocument(new PdfWriter(baos));
+			Document doc = new Document(pdfDoc);
+			doc.add(new Paragraph(String.format(
+					"You have submitted the following text using the %s method:",
+					request.getMethod())));
+			doc.add(new Paragraph(text));
+			doc.close();
+
+			// setting some response headers
+			response.setHeader("Expires", "0");
+			response.setHeader("Cache-Control",
+					"must-revalidate, post-check=0, pre-check=0");
+			response.setHeader("Pragma", "public");
+			// setting the content type
+			response.setContentType("application/pdf");
+			// the contentlength
+			response.setContentLength(baos.size());
+			// write ByteArrayOutputStream to the ServletOutputStream
+			OutputStream os = response.getOutputStream();
+			baos.writeTo(os);
+			os.flush();
+			os.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+		InputStream inputStream = new FileInputStream(new File(filePath));
+		InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentLength(Files.size(Paths.get(filePath)));
+		return new ResponseEntity(inputStreamResource, headers, HttpStatus.OK);
 	}
 }
