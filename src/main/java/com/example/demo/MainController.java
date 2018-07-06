a4c23754378be59b09edaacae9a7878239e2b265
GBTC441008UR <GBTC441008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-06 10:57:40 -0400

    cleaned and work

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -3,24 +3,13 @@ package com.example.demo;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 @Controller
 public class MainController {
@@ -45,16 +34,12 @@ public class MainController {
 		}else {
 
 			studentrepository.save(student);
-			model.addAttribute(student);
+			//model.addAttribute(student);
 			return"printRegistration";
 		}
 	}
 	@RequestMapping(value = "/download", method = RequestMethod.POST)
-	public ResponseEntity download(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("currStudent")Student student) throws IOException {
-        String studentName=student.getFirstname()+student.getLastname();
-        String home = System.getProperty("user.home");
-
-	    String filePath = home+"/Downloads/"+studentName+"RegForm.pdf";
+	public @ResponseBody String download(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("currStudent")Student student) throws IOException {
 
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -78,11 +63,6 @@ public class MainController {
 			e.printStackTrace();
 		}
 
-
-		InputStream inputStream = new FileInputStream(new File(filePath));
-		InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentLength(Files.size(Paths.get(filePath)));
-		return new ResponseEntity(inputStreamResource, headers, HttpStatus.OK);
+		return "printerPage";
 	}
 }
